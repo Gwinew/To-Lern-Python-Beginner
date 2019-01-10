35c666c56042c81458e7497f9ee2976ff71ce27b
Gwinew <maksymilian.wesolowski@gmail.com>
2019-01-11 00:37:36 +0100

    Add something to lesson 3

--- Tutorial_from_Flynerd/Lesson_3/Formatting_subtitles.py
+++ Tutorial_from_Flynerd/Lesson_3/Formatting_subtitles.py
@@ -1,2 +1,13 @@
 # -*- coding: utf-8 -*-
 
+#Formating Subtitles
+
+# We have some few way to formating floating record:
+# %f. ; {:f} ; round(x,y) - when x is argument and y is number of digits after the decimal point
+# for example (for x=2/3):
+x=4.0/3.0
+print(x)
+print("x is: %f." % x)
+print("x is {:f}" .format(x) )
+print("x is", round(x,6))
+
