bb9220f47347ec0ab7b00bed560fae906dd1160b
Maksymilian <maksymilian.wesolowski@gmail.com>
2019-08-01 14:54:41 +0200

    Update Formatting_subtitles.py

--- Tutorial_from_Flynerd/Lesson_3/Formatting_subtitles.py
+++ Tutorial_from_Flynerd/Lesson_3/Formatting_subtitles.py
@@ -6,7 +6,15 @@
 # The first is the old formatting used by python 2.0+
 # The secound and last is new formatting used by python 3.0+
 #
-# 
+# In the old formatting we use sign "%" inside sentence in place where we want put the argument and outside of sentence where we need to define the argument.
+# We cannot use "%" alone, we need use it with correctly type like "%s", "%f" etc.
+#
+# In the new formatting we use "{}" insted %. We can put "{}" alone or with type inside like "{:f}, {:s} etc.
+#
+# Type of subtitles:
+# s - string - inscription
+# d - digit - 
+#
 # We have some few way to formating floating record:
 # %f. ; {:f} ; round(x,y) - when x is argument and y is number of digits after the decimal point
 # for example (for x=2/3):
