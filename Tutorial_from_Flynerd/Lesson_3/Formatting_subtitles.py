9125d32f30086b2081f742611ef4990a4933002b
Maksymilian <maksymilian.wesolowski@gmail.com>
2019-08-01 14:33:11 +0200

    Update Formatting_subtitles.py

--- Tutorial_from_Flynerd/Lesson_3/Formatting_subtitles.py
+++ Tutorial_from_Flynerd/Lesson_3/Formatting_subtitles.py
@@ -2,6 +2,11 @@
 
 #Formating Subtitles
 
+# We have two standard type to formating subtitles:
+# The first is the old formatting used by python 2.0+
+# The secound and last is new formatting used by python 3.0+
+#
+# 
 # We have some few way to formating floating record:
 # %f. ; {:f} ; round(x,y) - when x is argument and y is number of digits after the decimal point
 # for example (for x=2/3):
@@ -11,3 +16,4 @@ print("x is: %f." % (x))
 print("x is {:f}" .format(x) )
 print("x is", round(x,6))
 
+# 
