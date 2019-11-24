2dfd23f7dfe53c53bcc141dd4a0718dd6ba93a25
Maksymilian <maksymilian.wesolowski@gmail.com>
2019-11-24 15:30:44 +0100

    Little changes
    Add few lines with comments

--- Tutorial_from_Flynerd/Lesson_3/Formatting_subtitles.py
+++ Tutorial_from_Flynerd/Lesson_3/Formatting_subtitles.py
@@ -13,7 +13,8 @@
 #
 # Type of subtitles:
 # s - string - inscription
-# d - digit - 
+# d - digit  - natural number
+# f - float  - floating point number
 #
 # We have some few way to formating floating record:
 # %f. ; {:f} ; round(x,y) - when x is argument and y is number of digits after the decimal point
@@ -24,4 +25,10 @@ print("x is: %f." % (x))
 print("x is {:f}" .format(x) )
 print("x is", round(x,6))
 
-# 
+# The first two options to formating records are formating strings - with this options you can formating every number or strings. It is something like translate digit format to string format.
+# The last option to formating records is reseve to work only with numbers.
+#
+# When we don't using the formating records for numbers like "float" then in original Python take this numbers and formating this in "double".
+# Double have 15 numbersof digits after the decimal point. Float have 6.
+#
+# Formating 
