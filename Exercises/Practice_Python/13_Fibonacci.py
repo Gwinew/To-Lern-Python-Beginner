6c9d0bcc9655cb726eef0f59a00a3b18efa6dbac
Gwinew <maksymilian.wesolowski@gmail.com>
2020-05-06 00:03:21 +0200

    add from atom2

--- Exercises/Practice_Python/13_Fibonacci.py
+++ Exercises/Practice_Python/13_Fibonacci.py
@@ -8,7 +8,7 @@ in the sequence to generate.
 the next number in sequence is the sum of previous thow numbers in the sequence.
 The sequence looks like this: 1, 1, 2, 3, 5, 8, 13, ...)
 """
-
+"""
 def ask():
     return int(input('Please give me a number:\n'))
 
@@ -23,3 +23,87 @@ def fibonacci(x):
 
 x=ask()
 print(fibonacci(x))
+"""
+
+# too slow, code no. 4 is better - remmember - alocate memory without go outside of the function.
+
+
+
+
+from timeit import timeit
+
+code_1 = """
+def gen_fib():
+    count = 10
+    i = 1
+    if count == 0:
+        fib = []
+    elif count == 1:
+        fib = [1]
+    elif count == 2:
+        fib = [1,1]
+    elif count > 2:
+        fib = [1,1]
+        while i < (count - 1):
+            fib.append(fib[i] + fib[i-1])
+            i += 1
+
+    return fib
+y = gen_fib()
+"""
+
+code_2 = """
+def fibonacci():
+    num = 10
+    i = 1
+    if num == 0:
+        fib = []
+    elif num == 1:
+        fib = [1]
+    elif num == 2:
+        fib = [1,1]
+    elif num > 2:
+        fib = [1,1]
+        while i < (num - 1):
+            fib.append(fib[i] + fib[i-1])
+            i += 1
+    return fib
+y=fibonacci()
+"""
+
+code_3 = """
+def fibonacci(x):
+    fibonacci = [1, 1]
+    for i in range(2, x):
+        a = fibonacci[i-2]
+        b = fibonacci[i-1]
+        c = a + b
+        fibonacci.append(c)
+    return fibonacci
+y=fibonacci(10)
+"""
+code_4 = """
+def fibonacci(n):
+    a = 1
+    b = 1
+    if n < 0:
+        print("Incorrect input")
+    elif n == 0:
+        fib = [1]
+    elif n == 1:
+        fib = [1, 1]
+    else:
+        fib = [1, 1]
+        for i in range(2,n):
+            c = a + b
+            a = b
+            b = c
+            fib.append(c)
+    return fib
+y = fibonacci(10)
+"""
+
+print('code 1: ', timeit(code_1))
+print('code 2: ', timeit(code_2))
+print('code 3: ', timeit(code_3))
+print('code 4: ', timeit(code_4))
---
+++ Exercises/Practice_Python/14_List_remove_Duplicates.py
@@ -0,0 +1,11 @@
+"""
+Write a program (function!) that takes a list and return a new list
+that contains all the elements of the first list minus all the duplicates.
+
+    Extras:
+        - Write two different function to do this:
+            one using a loop and constructing a list,
+            and another using sets.
+        - Go back and do Exercise 5 using sets, and write the solution
+            for that in a different function.
+"""
