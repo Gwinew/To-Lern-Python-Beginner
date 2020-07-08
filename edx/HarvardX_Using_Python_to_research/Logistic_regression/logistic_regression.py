2b429a074d126d256ff24850971bbcffdf828a80
Gwinew <maksymilian.wesolowski@gmail.com>
2020-07-08 17:24:16 +0200

    The Endo of time in edx research

--- edx/HarvardX_Using_Python_to_research/Logistic_regression/logistic_regression.py
+++ edx/HarvardX_Using_Python_to_research/Logistic_regression/logistic_regression.py
@@ -69,3 +69,26 @@ clf.score(X_test, y_test)
 
 clf.predict_proba(np.array([-2,0]).reshape(1,-1))
 clf.predict(np.array([-2,0]).reshape(1,-1))
+
+
+# Computing predictive Probabilities Across the Grid
+
+def plot_probs(ax, clf, class_no):
+    xx1, xx2 = np.meshgrid(np.arange(-5, 5, 0.1), np.arange(-5, 5, 0.1))
+    probs = clf.predict_proba(np.stack((xx1.ravel(), xx2.ravel()), axis=1))
+    Z = probs[:,class_no]
+    Z = Z.reshape(xx1.shape)
+    CS = ax.contourf(xx1, xx2, Z)
+    cbar = plt.colorbar(CS)
+    plt.xlabel("$X_1$")
+    plt.ylabel("$X_2$")
+
+
+
+plt.figure(figsize=(5,8))
+ax = plt.subplot(211)
+plot_probs(ax, clf, 0)
+plt.title("Pred. prob for class 1")
+ax = plt.subplot(212)
+plot_probs(ax, clf, 1)
+plt.title("Pred. prob for class 2");
